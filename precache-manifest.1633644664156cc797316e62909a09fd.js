88d0444ee0442f35f085af9272409151c7ec3a64
erkinaiSh <erkinai.shamshidinova@iaau.edu.kg>
2023-01-06 01:05:53 +0600

    Updates

---
+++ precache-manifest.1633644664156cc797316e62909a09fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c505144511e5e05b408102cba29dead",
+    "url": "/RandomCountry/index.html"
+  },
+  {
+    "revision": "5f6d1ca73327ca518947",
+    "url": "/RandomCountry/static/css/main.8c950fd1.chunk.css"
+  },
+  {
+    "revision": "b75377d708a3af48353b",
+    "url": "/RandomCountry/static/js/2.67d9d84c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/RandomCountry/static/js/2.67d9d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6d1ca73327ca518947",
+    "url": "/RandomCountry/static/js/main.c4a7c4b0.chunk.js"
+  },
+  {
+    "revision": "3438784eacb16d9af641",
+    "url": "/RandomCountry/static/js/runtime-main.3db6d02d.js"
+  },
+  {
+    "revision": "423c53a0ea2e99a0a112c5d47438557f",
+    "url": "/RandomCountry/static/media/romania-flag.423c53a0.jpg"
+  }
+]);
